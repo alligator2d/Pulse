37b5037b0c8588ac39421546fd9f42952f2e45ed
Oleg <alligator2d@rambler.ru>
2022-09-23 16:41:06 +0300

    second coomit

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.carousel__inner').slick({
+    speed: 1200,
+    slidesToShow: 1,
+    // adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.png"</button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.png"</button>',
+  });
+});

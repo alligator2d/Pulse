6f9cad1892f7a2e6736a2dea382534517119c6ba
Oleg <alligator2d@rambler.ru>
2022-09-24 11:59:34 +0300

    second coomit

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,31 @@ $(document).ready(function () {
     speed: 1200,
     slidesToShow: 1,
     // adaptiveHeight: true,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
     prevArrow:
       '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.png"</button>',
     nextArrow:
       '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.png"</button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+          dots: true,
+        },
+      },
+    ],
   });
 });
+
+// var slider = tns({
+//   container: '.carousel__inner',
+//   items: 1,
+//   slideBy: 'page',
+//   autoplay: false,
+//   controlsText: [
+//     'img src="icons/chevron-left-solid.png',
+//     'img src="icons/chevron-right-solid.png',
+//   ],
+// });
